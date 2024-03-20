07e41b8d83265c662c330e6a5aa71906a78b77d3
Joe Futrelle <jfutrelle@whoi.edu>
2024-03-20 17:02:49 -0400

    first cut, including s3 implementation

---
+++ src/storage/object.py
@@ -0,0 +1,81 @@
+from abc import ABC, abstractmethod
+from io import BytesIO
+
+"""
+The ObjectStore class is an abstract base class that defines the interface for an object store.
+
+The DictStore class is a simple implementation of an object store that stores objects in a dictionary in memory.
+"""
+
+class ObjectStore(ABC):
+    """
+    An abstract base class for an object store.
+    
+    An object store is a system for storing and retrieving byte arrays (objects) using a unique key.
+    
+    The key is a unique identifier for the object, and can be used to retrieve the object from the store.
+    
+    The object store provides methods for storing, retrieving, and deleting objects, as well as checking if an object exists in the store.
+    
+    The object store may also provide a method for listing the keys of all objects in the store.
+    
+    Stateful implementations of the object store should provide a context manager interface, allowing the store to be used in a with statement.
+    """
+    @abstractmethod
+    def get(self, key) -> bytearray:
+        """ return the data associated with the key"""
+        pass
+
+    @abstractmethod
+    def put(self, key, data: bytearray):
+        """ store the data associated with the key from the file-like object data. """
+        pass
+
+    @abstractmethod
+    def exists(self, key):
+        """ return True if the key exists in the store, False otherwise. """
+        pass
+
+    @abstractmethod
+    def delete(self, key):
+        """ delete the data associated with the key. """
+        pass
+
+    def keys(self):
+        """" return an iterable of keys for all objects in the store.
+        classes that do not support listing keys should raise a NotImplementedError """
+        raise NotImplementedError
+
+    
+class DictStore(ObjectStore):
+    """
+    Stores data in a dictionary in memory.
+    """
+    def __init__(self):
+        self.objects = {}
+
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
+
+    def get(self, key):
+        bytes = self.objects.get(key)
+        if bytes is None:
+            raise KeyError(key)
+        return bytes
+
+    def put(self, key, data):
+        self.objects[key] = data
+
+    def exists(self, key):
+        return key in self.objects
+
+    def delete(self, key):
+        if not self.exists(key):
+            raise KeyError(key)
+        del self.objects[key]
+
+    def keys(self):
+        return self.objects.keys()
---
+++ src/storage/s3.py
@@ -0,0 +1,117 @@
+import boto3
+import aiobotocore
+from aiobotocore.session import get_session
+
+from .object import ObjectStore
+
+
+class BucketStore(ObjectStore):
+    def __init__(self, s3_url, s3_access_key, s3_secret_key, bucket_name):
+        self.s3_url = s3_url
+        self.s3_access_key = s3_access_key
+        self.s3_secret_key = s3_secret_key
+        self.bucket_name = bucket_name
+        self.session = None
+        self.s3_client = None
+
+    def __enter__(self):
+        self.session = boto3.session.Session()
+        self.s3_client = self.session.client(
+            's3',
+            endpoint_url=self.s3_url,
+            aws_access_key_id=self.s3_access_key,
+            aws_secret_access_key=self.s3_secret_key
+        )
+        return self
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.s3_client.close()
+
+    def put(self, key, data):
+        self.s3_client.put_object(
+            Bucket=self.bucket_name,
+            Key=key,
+            Body=data
+        )
+        return True
+    
+    def get(self, key):
+        response = self.s3_client.get_object(
+            Bucket=self.bucket_name,
+            Key=key
+        )
+        file_contents = response['Body'].read()
+        return file_contents
+    
+    def exists(self, key):
+        try:
+            self.s3_client.head_object(
+                Bucket=self.bucket_name,
+                Key=key
+            )
+            return True
+        except self.s3_client.exceptions.NoSuchKey:
+            return False
+        
+    def delete(self, key):
+        self.s3_client.delete_object(
+            Bucket=self.bucket_name,
+            Key=key
+        )
+        return True
+    
+    def keys(self, prefix=''):
+        response = self.s3_client.list_objects_v2(
+            Bucket=self.bucket_name,
+            Prefix=prefix
+        )
+        keys = [obj['Key'] for obj in response['Contents']]
+        return keys
+
+
+class AsyncBucketStore(ObjectStore):
+    def __init__(self, s3_client, bucket_name):
+        self.bucket_name = bucket_name
+        self.s3_client = s3_client
+
+    async def put(self, key, data):
+        await self.s3_client.put_object(
+            Bucket=self.bucket_name,
+            Key=key,
+            Body=data
+        )
+        return True
+    
+    async def get(self, key):
+        response = await self.s3_client.get_object(
+            Bucket=self.bucket_name,
+            Key=key
+        )
+        file_contents = await response['Body'].read()
+        return file_contents
+    
+    async def exists(self, key):
+        try:
+            await self.s3_client.head_object(
+                Bucket=self.bucket_name,
+                Key=key
+            )
+            return True
+        except self.s3_client.exceptions.NoSuchKey:
+            return False
+        
+    async def delete(self, key):
+        response = await self.s3_client.delete_object(
+            Bucket=self.bucket_name,
+            Key=key
+        )
+        return True
+    
+    async def keys(self, prefix=''):
+        # FIXME paginate
+        response = await self.s3_client.list_objects_v2(
+            Bucket=self.bucket_name,
+            Prefix=prefix
+        )
+        keys = [obj['Key'] for obj in response['Contents']]
+        return keys
